7849fcaf8b2d90723ffa5127b4e3b008efc609de
Hernan Alvarado <hernanvid123@gmail.com>
2025-01-21 22:36:15 +0000

    chore: initial setup (#1)
    * Initial commit from Create Next App
    
    * chore: initial setup

---
+++ README.md
@@ -0,0 +1,1 @@
+# Next Banking Company
---
+++ src/app/layout.tsx
@@ -0,0 +1,19 @@
+import { Lexend } from "next/font/google";
+import { ChildrenProps } from "@/lib/@types/props";
+import "@/ui/globals.css";
+
+const lexend = Lexend({
+  weight: ["400", "500", "600", "700"],
+  subsets: ["latin"],
+  variable: "--font-lexend",
+});
+
+export default function RootLayout({ children }: ChildrenProps) {
+  return (
+    <html lang="en">
+      <body className={`${lexend.className} antialiased bg-grey-1100`}>
+        {children}
+      </body>
+    </html>
+  );
+}
